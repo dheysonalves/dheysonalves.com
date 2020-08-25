5dfc2fb86cd92e5370d70cf2a3bbf99403e1dd1d
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-08-25 08:55:39 -0300

    feat: Add pagination component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import * as S from './styles';
 
 import Image from '../Image/index';
@@ -8,26 +9,46 @@ import Subtitle from '../Subtitle/index';
 import { AiFillGithub, AiFillEye } from "react-icons/ai";
 
 
-const Card = ({ margin }) => {
+const Card = ({ margin, ...data }) => {
+	const { title, subtitle, github, linkedin, photo } = data;
 	return (
 		<S.Wrapper margin={margin}>
-			<Image source="https://source.unsplash.com/random" alt="random image" />
+			<Image source={photo} alt="random image" />
 			<Title>
-				Teste
+				{title}
 			</Title>
 			<Subtitle>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit...
-		</Subtitle>
+				{subtitle}
+			</Subtitle>
 			<S.IconsWrapper>
-				<S.IconLink>
+				<S.IconLink href={github}>
 					<AiFillGithub size={24} />
 				</S.IconLink>
-				<S.IconLink>
+				<S.IconLink href={linkedin}>
 					<AiFillEye size={24} />
 				</S.IconLink>
 			</S.IconsWrapper>
 		</S.Wrapper>
 	)
 }
+
+Card.propTypes = {
+	data: PropTypes.shape({
+		photo: PropTypes.string.isRequired,
+		title: PropTypes.string.isRequired,
+		subtitle: PropTypes.string.isRequired,
+		github: PropTypes.string,
+		linkedin: PropTypes.string
+	})
+
+};
+
+Card.defaultProps = {
+	photo: 'https://source.unsplash.com/random/400x400',
+	title: 'Portfolio',
+	subtitle: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus architecto voluptas dicta laborum optio blanditiis at dolores nobis quasi cum vel labore, voluptates enim asperiores, quis nemo facere. Aspernatur, amet!',
+	github: 'https://github.com/Dheyson',
+	linkedin: 'https://www.linkedin.com/in/dheysonalvess/'
+};
 
 export default Card;
--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,11 +1,59 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useCallback, useState} from 'react';
 
 import * as S from './styles';
 
 import Card from '../Card';
+import Pagination from '../../utils/pagination';
+
+const values = [
+	{
+		photo: '',
+		title: '',
+		subtitle: '',
+		github: '',
+		linkedin: '',
+	},
+	{
+		photo: '',
+		title: '',
+		subtitle: '',
+		github: '',
+		linkedin: '',
+	},
+	{
+		photo: '',
+		title: '',
+		subtitle: '',
+		github: '',
+		linkedin: '',
+	},
+	{
+		photo: '',
+		title: '',
+		subtitle: '',
+		github: '',
+		linkedin: '',
+	}
+]
 
 const CardList = () => {
+	// const [state, setState] = useState({ allRepositories: [], currentRepositories: [], currentPage: null, totalPages: null });
+
+	// useEffect(() => {
+	// 	const { data: allRepositories = [] } = values.findAll();
+	// 	setState({ allRepositories });
+	// }, [setState]);
+
+	// const onPageChanged = useCallback((data) => {
+	// 	const { allRepositories } = state;
+	// 	const { currentPage, totalPages, pageLimit } = data;
+
+	// 	const offset = (currentPage - 1) * pageLimit;
+	// 	const currentRepositories = allRepositories.slice(offset, offset + pageLimit);
+
+	// 	setState({ currentPage, currentRepositories, totalPages });
+	// }, [state, setState]);
+
 	return (
 		<S.Wrapper>
 			<Card margin={20} />

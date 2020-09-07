cabfa900eb647a89d05d5cb9a827c1d5d9d3eaba
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-09-07 18:16:33 -0300

    Remove unused stories

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,16 +1,16 @@
-export const theme = {
-           light: {
-               font: '#292F36',
-               background: '#ffffff',
-               links: '#57886C',
-               links_hover: '#363636',
-               scroll: '#16191E',
-           },
-           dark: {
-               font: '#ffffff',
-               background: '#16191E',
-               links: '#57886C',
-               links_hover: '#AEFFD8',
-               scroll: '#ffffff',
-           },
-       };
+export default theme = {
+    light: {
+        font: '#292F36',
+        background: '#ffffff',
+        links: '#57886C',
+        links_hover: '#363636',
+        scroll: '#16191E',
+    },
+    dark: {
+        font: '#ffffff',
+        background: '#16191E',
+        links: '#57886C',
+        links_hover: '#AEFFD8',
+        scroll: '#ffffff',
+    },
+};

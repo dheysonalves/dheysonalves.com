a02c29a733b0466976cdda221c72014b9c73c257
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-09-09 21:30:46 -0300

    fix: Props mistakes in 404 webpage

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,12 +4,12 @@ import { graphql } from 'gatsby';
 import Layout from '../components/Layout/layout';
 import SEO from '../components/Seo/Seo';
 
-const NotFoundPage= () => {
-        const { data } = this.props;
+const NotFoundPage= ({data, location}) => {
+
         const siteTitle = data.site.siteMetadata.title;
 
         return (
-            <Layout location={this.props.location} title={siteTitle}>
+            <Layout location={location} title={siteTitle}>
                 <SEO title="404: Não encontrado" />
                 <h1>Não encontrado</h1>
                 <p>Você clicou em uma rota que não existe... uma pena.</p>

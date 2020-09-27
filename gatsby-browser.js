67bf3291366259a83f859238b2541af0f02de357
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-09-26 23:30:58 -0300

    [i18n] Internationalize for english and portuguese Fixes #23

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import GlobalStateProvider from './src/store/GlobalStateProvider';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme/theme';
 // custom typefaces
 import "typeface-montserrat";
 import "typeface-merriweather";
+import './i18n';
 
 export const wrapRootElement = ({ element }) => (
 	<GlobalStateProvider>
 		<ThemeProvider theme={theme}>
-			{element}
+			<Suspense fallback="loading">
+				{element}
+			</Suspense>
 		</ThemeProvider>
 	</GlobalStateProvider>
 )

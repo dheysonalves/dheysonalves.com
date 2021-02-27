3f6f0c1d8b27c819e25d88b407479abdb818d4fa
Dheyson Alves <dheyson.lucas@hotmail.com>
2021-02-27 16:39:27 -0300

    fix: lodash _ import fixed

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,5 @@
-/* eslint-disable no-undef */
 const path = require(`path`);
+const _ = require('lodash');
 const { createFilePath } = require(`gatsby-source-filesystem`);
 
 exports.createPages = async ({ graphql, actions }) => {

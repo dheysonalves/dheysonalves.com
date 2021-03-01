768c7c0c8053726dbb91fe16975fdb34f731667a
Dheyson Alves <dheyson.lucas@hotmail.com>
2021-02-28 23:09:35 -0300

    fix: mobile screen and tablet blog position

--- src/components/Layout/Main/styles.js
+++ src/components/Layout/Main/styles.js
@@ -5,10 +5,6 @@ export const Main = styled.main`
     max-width: ${(props) => props.max || rhythm(20)};
     margin: ${(props) => props.mw && '0 auto'};
 
-    @media (max-width: 800px) {
-        max-width: ${rhythm(100)};
-        align-self: center;
-    }
     @media (min-width: 800px) {
         align-self: center;
     }

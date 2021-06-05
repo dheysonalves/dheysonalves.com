acbd7c5b8c4e9bf558079140353010cc5a2a384c
Dheyson Alves <dheyson.lucas@hotmail.com>
2021-06-05 13:33:57 -0300

    feat: add button component testing

--- src/components/Button/test.jsx
+++ src/components/Button/test.jsx
@@ -4,13 +4,13 @@ import { render, screen } from '@testing-library/react';
 import Button from '.';
 
 describe('<Button />', () => {
-    it('should render the heading', () => {
-        const { container } = render(<Button />);
+	it('should render the `Wanna share an idea?` text', () => {
+		const { container } = render(<Button text="testing" />);
 
-        expect(
-            screen.getByRole('heading', { name: /Button/i })
-        ).toBeInTheDocument();
-
-        expect(container.firstChild).toMatchSnapshot();
-    });
+		expect(
+			screen.getByRole('button', { name: /Wanna share an idea?/i })
+		).toBeInTheDocument();
+		expect(screen.getByRole('button')).not.toBeDisabled();
+		expect(container.firstChild).toMatchSnapshot();
+	});
 });

e7aeef6a32ae742e2b10a024c781dcbac394e960
Dheyson Alves <dheyson.lucas@hotmail.com>
2022-05-19 13:10:15 -0300

    feat: update header portfolio link

--- src/components/Layout/Navigation/Navigation.jsx
+++ src/components/Layout/Navigation/Navigation.jsx
@@ -5,74 +5,74 @@ import { FaLightbulb, FaRegLightbulb } from 'react-icons/fa';
 import * as S from './Navigation.styles';
 
 const Navigation = ({ sticky }) => {
-    const [iconState, setIconState] = useState();
-    const { state, dispatch } = useContext(Context);
+	const [iconState, setIconState] = useState();
+	const { state, dispatch } = useContext(Context);
 
-    const dispatching = useCallback(() => {
-        setIconState(!iconState);
-        dispatch({ type: 'TOOGLE_DARK_MODE' });
-    }, [dispatch, iconState]);
+	const dispatching = useCallback(() => {
+		setIconState(!iconState);
+		dispatch({ type: 'TOOGLE_DARK_MODE' });
+	}, [dispatch, iconState]);
 
-    return (
-        <S.Header>
-            <S.Navigation>
-                <S.Title title="Opa, e ai?">DHEYSON ALVES</S.Title>
-            </S.Navigation>
-            <S.Menu
-                color={state.isDark ? '#292F36' : '#fff'}
-                link={state.isDark ? '#fff' : '#363636'}
-            >
-                <S.MenuItem>
-                    <a href="/" title="Página principal">
-                        Home
-                    </a>
-                </S.MenuItem>
-                <S.MenuItem>
-                    <a href="/writing/" title="Alguns artigos que escrevi">
-                        Escrita
-                    </a>
-                </S.MenuItem>
-                <S.MenuItem>
-                    <a
-                        href="https://dheyson10.gitbook.io/breakpoint/"
-                        title="Minha documentação oficial"
-                        target={`_blank`}
-                        rel={`noopener noreferrer`}
-                    >
-                        Documentação
-                    </a>
-                </S.MenuItem>
-                <S.MenuItem>
-                    <a
-                        href="https://github.com/dheysonalves"
-                        title="Meus atuais projetos"
-                        target={`_blank`}
-                        rel={`noopener noreferrer`}
-                    >
-                        Portfólio
-                    </a>
-                </S.MenuItem>
-                <S.CursorBulb>
-                    {state.isDark ? (
-                        <FaRegLightbulb
-                            onClick={() => dispatching()}
-                            size={32}
-                            title="Que tal acender a luz?"
-                        />
-                    ) : (
-                        <FaLightbulb
-                            onClick={() => dispatching()}
-                            size={32}
-                            title="Que tal apagar a luz?"
-                        />
-                    )}
-                </S.CursorBulb>
-                {/* <S.MenuItem>
+	return (
+		<S.Header>
+			<S.Navigation>
+				<S.Title title="Opa, e ai?">DHEYSON ALVES</S.Title>
+			</S.Navigation>
+			<S.Menu
+				color={state.isDark ? '#292F36' : '#fff'}
+				link={state.isDark ? '#fff' : '#363636'}
+			>
+				<S.MenuItem>
+					<a href="/" title="Página principal">
+						Home
+					</a>
+				</S.MenuItem>
+				<S.MenuItem>
+					<a href="/writing/" title="Alguns artigos que escrevi">
+						Escrita
+					</a>
+				</S.MenuItem>
+				<S.MenuItem>
+					<a
+						href="https://dheyson10.gitbook.io/breakpoint/"
+						title="Minha documentação oficial"
+						target={`_blank`}
+						rel={`noopener noreferrer`}
+					>
+						Documentação
+					</a>
+				</S.MenuItem>
+				<S.MenuItem>
+					<a
+						href="https://dheysonalvesportfolio.netlify.app/"
+						title="Meus atuais projetos"
+						target={`_blank`}
+						rel={`noopener noreferrer`}
+					>
+						Portfólio
+					</a>
+				</S.MenuItem>
+				<S.CursorBulb>
+					{state.isDark ? (
+						<FaRegLightbulb
+							onClick={() => dispatching()}
+							size={32}
+							title="Que tal acender a luz?"
+						/>
+					) : (
+						<FaLightbulb
+							onClick={() => dispatching()}
+							size={32}
+							title="Que tal apagar a luz?"
+						/>
+					)}
+				</S.CursorBulb>
+				{/* <S.MenuItem>
                         <Link to="/about/about">About</Link>
                     </S.MenuItem> */}
-            </S.Menu>
-        </S.Header>
-    );
+			</S.Menu>
+		</S.Header>
+	);
 };
 
 export default Navigation;

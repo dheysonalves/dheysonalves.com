37a6dc6cce92ea386e13585ae86084fa33749326
Dheyson Alves <dheyson.lucas@hotmail.com>
2022-07-06 15:18:19 -0300

    style: updating ships styles

--- src/components/Ships/index.jsx
+++ src/components/Ships/index.jsx
@@ -4,37 +4,38 @@ import PropTypes from 'prop-types';
 import * as S from './styles';
 
 const Ships = ({ label, color, radius, labelColor, ...props }) => {
-    return (
-        <S.ShipListWrapper>
-            <S.ShipWrapper color={color} radius={radius} {...props}>
-                <S.ShipText labelColor={labelColor}>{label}</S.ShipText>
-            </S.ShipWrapper>
-        </S.ShipListWrapper>
-    );
+	return (
+		<S.ShipListWrapper>
+			<S.ShipWrapper color={color} radius={radius} {...props}>
+				<S.ShipText labelColor={labelColor}>{label}</S.ShipText>
+				<S.ShipBackground color={color} />
+			</S.ShipWrapper>
+		</S.ShipListWrapper>
+	);
 };
 
 Ships.propTypes = {
-    /**
-     * Sets the label from the inner text (required)
-     */
-    label: PropTypes.string.isRequired,
-    /**
-     * Sets the background color for the ship (required)
-     */
-    color: PropTypes.string.isRequired,
-    /**
-     * Sets the radios for the ship
-     */
-    radius: PropTypes.bool,
-    /**
-     * Sets the label color for the ship
-     */
-    labelColor: PropTypes.string,
+	/**
+	 * Sets the label from the inner text (required)
+	 */
+	label: PropTypes.string.isRequired,
+	/**
+	 * Sets the background color for the ship (required)
+	 */
+	color: PropTypes.string.isRequired,
+	/**
+	 * Sets the radios for the ship
+	 */
+	radius: PropTypes.bool,
+	/**
+	 * Sets the label color for the ship
+	 */
+	labelColor: PropTypes.string,
 };
 
 Ships.defaultProps = {
-    label: 'React',
-    radius: false,
+	label: 'React',
+	radius: false,
 };
 
 export default Ships;
--- src/components/Ships/styles.js
+++ src/components/Ships/styles.js
@@ -1,11 +1,38 @@
 import styled from 'styled-components';
 
 export const ShipWrapper = styled.div`
+	position: relative;
+	display: inline-block;
+	box-shadow: inset 0 0 0 0.5px ${(props) => props.border};
+	border-radius: ${(props) => (props.radius ? '4px' : '1em')};
+	width: 140px;
+	margin-right: 0.5em;
+	margin-bottom: 1.5em;
+	cursor: pointer;
+	padding: 5px;
+	background-color: transparent;
+
+	&:hover {
+		-webkit-transition: background-color 500ms ease-out;
+		-moz-transition: background-color 500ms ease-out;
+		-o-transition: background-color 500ms ease-out;
+		transition: background-color 500ms ease-out;
+		background-color: ${(props) => props.color};
+	}
+`;
+
+export const ShipBackground = styled.span`
+	position: absolute;
+	content: '';
+	left: 0;
+	top: 0;
+	border-top-left-radius: 1em;
+	border-bottom-left-radius: 1em;
+	width: 10%;
+	height: 100%;
+	z-index: 2;
 	background-color: ${(props) => props.color};
-	border-radius: ${(props) => (props.radius ? '4px' : '')};
-	width: 120px;
-	margin-right: 5px;
-	margin-bottom: 5px;
+	box-shadow: inset 0 0 0 0.5px ${(props) => props.border};
 `;
 
 export const ShipListWrapper = styled.div`
@@ -15,7 +42,6 @@ export const ShipListWrapper = styled.div`
 export const ShipText = styled.p`
 	text-align: center;
 	font-size: 0.7rem;
-	padding: 2px;
 	margin: 0;
 	color: ${(props) => props.labelColor};
 	font-weight: 900;

fa7d3e47f22344063e90aa28489eff9de3754297
Dheyson Alves <dheyson.lucas@hotmail.com>
2022-07-07 16:47:24 -0300

    feat: adding reading-time counter

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -2,3 +2,11 @@ export const disqusConfig = ({ slug, title }) => ({
 	shortname: process.env.GATSBY_DISQUS_NAME,
 	config: { identifier: slug, title },
 });
+
+export const readingTime = (text) => {
+	const wpm = 225;
+	const words = text.trim().split(/\s+/).length;
+	const time = Math.ceil(words / wpm);
+
+	return time;
+};

e88c2435a025505d4be8d3b5ad4fb60947cb9df1
Dheyson Alves <dheyson.lucas@hotmail.com>
2022-07-07 22:38:57 -0300

    styles: updating footer styles

--- src/components/Layout/Footer/footer.jsx
+++ src/components/Layout/Footer/footer.jsx
@@ -1,64 +1,13 @@
 import React from 'react';
 
-import { IoLogoLinkedin, IoLogoGithub, IoLogoTwitter } from 'react-icons/io';
-import { FaDev, FaDiscord } from 'react-icons/fa';
 import * as S from './footer.styles';
+import BiographSocialMedia from '../../BiographSocialMedia';
 
 const Footer = ({ element }) => {
 	return (
 		<S.Footer ref={element}>
 			DHEYSON L.ALVES © {new Date().getFullYear()} • POWERED BY GATSBY
-			<S.IconsWrapper>
-				<a
-					href="https://www.linkedin.com/in/dheysonalvess/"
-					target="_blank"
-					rel="noopener noreferrer"
-					alt="Logo do Linkedin rede social"
-					title="Minha rede profissional"
-				>
-					<IoLogoLinkedin size="1.5em" />
-				</a>
-				<a
-					href="https://twitter.com/dheysonlalves"
-					target="_blank"
-					rel="noopener noreferrer"
-					className="anchor__specific"
-					alt="Logo do passado com cor branca do twitter"
-					title="Quer trocar uma ideia?, manda dm"
-				>
-					<IoLogoTwitter size="1.5em" />{' '}
-				</a>
-				<a
-					href="https://github.com/dheysonalves"
-					target="_blank"
-					rel="noopener noreferrer"
-					className="anchor__specific"
-					alt="social media"
-					title="Boa parte dos meus projetos"
-				>
-					<IoLogoGithub size="1.5em" />{' '}
-				</a>
-				<a
-					href="https://dev.to/dheyson_alvess"
-					target="_blank"
-					rel="noopener noreferrer"
-					className="anchor__specific"
-					alt="Logo quadrada com fundo preto com texto DEV no meio de cor branca"
-					title="Meus outros artigos em inglês"
-				>
-					<FaDev size="1.5em" />{' '}
-				</a>
-				<a
-					href="https://discord.gg/Fzk2PBB"
-					target="_blank"
-					rel="noopener noreferrer"
-					className="anchor__specific"
-					alt="Logo quadrada com fundo preto com texto DEV no meio de cor branca"
-					title="Comunidade do discord da Coffer"
-				>
-					<FaDiscord size="1.5em" />{' '}
-				</a>
-			</S.IconsWrapper>
+			<BiographSocialMedia logoSize="1.5em" />
 		</S.Footer>
 	);
 };
--- src/components/Layout/Footer/footer.styles.js
+++ src/components/Layout/Footer/footer.styles.js
@@ -10,22 +10,3 @@ export const Footer = styled.footer`
 	margin: 0 auto;
 	text-align: center;
 `;
-
-export const IconsWrapper = styled.div`
-	margin-top: 10px;
-	a {
-		color: #ff6100;
-		text-decoration: none;
-
-		:hover {
-			color: #363636;
-		}
-
-		:active {
-			color: #000007;
-		}
-	}
-	.anchor__specific {
-		padding-left: 10px;
-	}
-`;
